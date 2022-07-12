c5a4ecc5db2889bb08039370c841d3a73c76a093
Rakhilpr96 <rakhilpr96@gmail.com>
2022-07-12 22:40:22 +0530

    Updated JWT

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -29,7 +29,7 @@ const onLogin = () => {
   if (email == "user@email.com" && password == "1234") {
     const response = {
       httpStatus: "OK",
-      tokenX:
+      token:
         "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImxwdXNlcjAiLCJ1c2VySWQiOiIxMjYxMzAiLCJlbWFpbCI6InVzZXIxMjBAZW1haWwuY29tIiwiaWF0IjoxNjU2NjE2NzE4LCJleHAiOjE2ODgxNTI3MTgsImlzcyI6Imh0dHBzOi8vZXgtYnV5Lmhlcm9rdWFwcC5jb20iLCJzdWIiOiIxMjYxMzAifQ.Om-72nHSquTIJPbSJ0IMueWRTx8H8ks5BaZDxewpdY4yYyFS987mbZJY5C4bFsWSxo33yIVfsngWERzHloXqsostdR3BGmEZ3_AM14gxcJsn2RYHANKDaKjZc2d9S255aEp4w5SX-Z5QMMAmYMDw5FAl8uZ3KUaWjXlGwHKAzwm7xME2jNElIOYgFc1h-sBvcGGnaKKHHAEVWBpPJ_Na8dwYYgjA4d-T09dZPRJdAcsDtNNB4RlIrG7RaIyUOBrD9RNAtnakncJAtXj_juep0YqiTFJMdSDVqjQ5HdAoD7dJNISo9j2_hiRgjbIdJB7plvKgeZ0sf31zNHEwEptY6Q",
       id: "126130",
       iss: "https://ex-buy.herokuapp.com",
